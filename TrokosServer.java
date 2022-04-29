920c8fe29fb442ac54b6c584145c317d5fbf66d1
fc55228 <fc55228@alunos.fc.ul.pt>
2022-04-29 15:40:07 +0100

    final commit

--- TrokosServer.java
+++ TrokosServer.java
@@ -27,7 +27,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-// import java.io.Serial;
 import java.io.Serializable;
 import java.net.Socket;
 
